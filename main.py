42a94947ed5717f03d3e28161fb81b9bbad94c85
cmv520123 <107668987+cmv520123@users.noreply.github.com>
2022-06-21 16:15:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
-import requests # 訪問
+from flask import Flask, request, jsonify,render_template,url_for
+from flask_cors import CORS
+import numpy as np
+import requests as req
+import pandas as pd
+from requests_html import HTML
+import re
+import random
+import time
+import json
+from bs4 import BeautifulSoup
+from openpyxl import Workbook
 import jieba
 from collections import Counter # 次數統計
-import re
 from tkinter import font
 import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
 from matplotlib import colors
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator  
-import numpy as np
 from PIL import Image # 圖片轉array陣列
 import pandas as pd
 import seaborn as sns
 import os
 import csv
-from bs4 import BeautifulSoup
-from openpyxl import Workbook
 import parsel #數據解析模組
-import requests #數據請求模組
-import csv
-import json
 from openpyxl.cell.cell import ILLEGAL_CHARACTERS_RE
-import re
-import pandas as pd
 from json.decoder import JSONDecodeError
-import os
-import numpy as np
 
 app = Flask(__name__)
 CORS(app)
