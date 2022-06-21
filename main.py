3b3473fb9287f3a3037c89610535e5cbbba03795
cmv520123 <107668987+cmv520123@users.noreply.github.com>
2022-06-21 17:11:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from flask_cors import CORS
 import numpy as np
 import requests as req
 import pandas as pd
-
+from requests_html import HTML
+import re
+import random
+import time
+import json
+from bs4 import BeautifulSoup
+from openpyxl import Workbook
 from bs4 import BeautifulSoup
 from openpyxl import Workbook
 import parsel #數據解析模組
@@ -33,12 +39,12 @@ def index():
 def DATA():
     if request.method =='POST':
         if request.values['send']=='送出':
+      
             keyword=request.values['user']
             from crawler_all import crawler_all
             crawler_all(keyword=keyword)
+            
             return render_template('data.html',name="下載成功自己去找!!")
-        return render_template('data.html',name="")
-    
-    
+    return render_template('data.html',name="")
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=80)
+    app.run()
